8003fce4ce5d46e827cd8b4ea5de421a27c945df
Team 9567 <Team9567@gmail.com>
2025-10-09 19:30:26 -0700

    Remapped buttons
    remapped buttons and added a custom keybind to move forwards
    for a set amount of time, at a set speed.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,15 +8,20 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.TabiSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;//CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
@@ -24,22 +29,28 @@ public class RobotContainer {
   private final TabiSubsystem m_exampleSubsystem = new TabiSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandGenericHID m_driverController =
-      new CommandGenericHID(OperatorConstants.kDriverControllerPort);
+  private final CommandGenericHID m_driverController = new CommandGenericHID(OperatorConstants.kDriverControllerPort);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
@@ -47,16 +58,24 @@ public class RobotContainer {
     new Trigger(m_exampleSubsystem::exampleCondition)
         .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
+    // pressed,
     // cancelling on release.
-    m_driverController.button(OperatorConstants.kDriverControllerA).whileTrue(m_exampleSubsystem.exampleMethodCommand());
-    m_exampleSubsystem.setDefaultCommand(
-      new RunCommand(
+    m_driverController.button(OperatorConstants.kDriverControllerA).whileTrue(m_exampleSubsystem.run(
         () -> {
-          m_exampleSubsystem.arcadeDrive(-m_driverController.getRawAxis(OperatorConstants.kControllerLeftVertical), 
-          -m_driverController.getRawAxis(OperatorConstants.kControllerLeftHorizontal));
-        }, m_exampleSubsystem)
-    );
+          m_exampleSubsystem.arcadeDrive(0.15, 0);
+        }
+
+    ).withTimeout(2.0));
+    m_exampleSubsystem.setDefaultCommand(
+        new RunCommand(
+            () -> {
+              double gamepadDrive = m_driverController.getRawAxis(OperatorConstants.kControllerLeftVertical);
+              double gamepadTurn = m_driverController.getRawAxis(OperatorConstants.kControllerLeftHorizontal);
+              SmartDashboard.putNumber("gamepadDrive", gamepadDrive);
+              SmartDashboard.putNumber("gamepadTurn", gamepadTurn);
+              m_exampleSubsystem.arcadeDrive(-gamepadDrive, -gamepadTurn);
+            }, m_exampleSubsystem));
   }
 
   /**
