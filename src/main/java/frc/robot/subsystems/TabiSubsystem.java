d21fcff88fda79563800d474d07375c31e8428b1
Team 9567 Programming <148642470+Team9567Prog@users.noreply.github.com>
2026-04-20 16:48:31 -0700

    Updating project to WPILib 2026

--- src/main/java/frc/robot/subsystems/TabiSubsystem.java
+++ src/main/java/frc/robot/subsystems/TabiSubsystem.java
@@ -9,8 +9,8 @@ import static edu.wpi.first.units.Units.Meters;
 
 import java.util.Optional;
 
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
